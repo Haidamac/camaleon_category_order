284eead19489844d45c15a7031fe93ec6f50e9dd
superchel <for.oleg.mozolev@gmail.com>
2018-04-23 12:41:27 +0300

    added support for multi-level structure categories

--- app/assets/javascripts/plugins/camaleon_category_order/category_reorder.js
+++ app/assets/javascripts/plugins/camaleon_category_order/category_reorder.js
@@ -1,66 +1,71 @@
 (function($){
-    $.fn.reorder = function (options){
-        var default_options = {url: "", table: ".table"};
-        options = $.extend(default_options, options || {});
+  $.fn.reorder = function (options){
+    var default_options = {url: "", table: ".table"};
+    options = $.extend(default_options, options || {});
 
-        var th_data = false;
+    var th_data = false;
 
-        $(options.table+' thead tr').each(function(i, el) {
+    $(options.table+' thead tr').each(function(i, el) {
 
-            th = $(this).find('th').attr('data-sortable');
+      th = $(this).find('th').attr('data-sortable');
 
-            if(typeof th === "undefined"){
-                th_data = true
-            }else{
-                th_data = false
-            }
+      if(typeof th === "undefined"){
+        th_data = true
+      }else{
+        th_data = false
+      }
 
-        });
+    });
 
-        if(th_data){
-            th_new = '<th class="center" data-sortable="0"></th>';
-            $(options.table+' thead tr').prepend(th_new);
+    if(th_data){
+      th_new = '<th class="center" data-sortable="0"></th>';
+      $(options.table+' thead tr').prepend(th_new);
 
-            $(options.table+' tbody tr').each(function(i, el) {
-                id = $(this).attr('data-id');
-                $(this).addClass('sortable');
+      $(options.table+' tbody tr').each(function(i, el) {
 
-                td_new = '<td>'
-                            +'<div class="moved" style="cursor: all-scroll">'
-                            +'<i class="fa fa-arrows"></i>'
-                            +'<input type="hidden" name="values[]" value="'+id+'" />'
-                            +'</div>'
-                        '</td>';
+        if ($(this).text().indexOf('uncategorized') < 0) {
+          id = $(this).attr('data-id');
+          $(this).addClass('sortable');
 
-                $(this).prepend(td_new);
+          td_new = '<td>'
+            +'<div class="moved" style="cursor: all-scroll">'
+            +'<i class="fa fa-arrows"></i>'
+            +'<input type="hidden" name="values[]" value="'+id+'" />'
+            +'</div>'
+          '</td>';
 
-            });
+          $(this).prepend(td_new);
+        }
+        else {
+          $(this).prepend('<td></td>');
         }
 
+      });
+    }
 
-        $( options.table+" tbody" ).sortable({
-            axis: "y",
-            placeholder: "ui-state-highlight",
-            handle: ".moved",
-            //items: "tr:not(.sortable)",
-            items: "tr.sortable",
-            start: function(event, ui) {
-                ui.item.startPos = ui.item.index();
 
-            },
-            stop: function( event, ui ) {
-                showLoading();
-                $.post(options.url, $(options.table+" input" ).serialize(), function(){
-                    if(ui.item.startPos != ui.item.index()){
-                        hideLoading();
-                    }
-                });
-            },
-            change: function(event, ui) {
-            }
+    $( options.table+" tbody" ).sortable({
+      axis: "y",
+      placeholder: "ui-state-highlight",
+      handle: ".moved",
+      //items: "tr:not(.sortable)",
+      items: "tr.sortable",
+      start: function(event, ui) {
+        ui.item.startPos = ui.item.index();
+      },
+      stop: function( event, ui ) {
+        showLoading();
+        $.post(options.url, $(options.table).find('[data-parent=' + ui.item.data('parent') + '] input').serialize(), function(){
+          if(ui.item.startPos != ui.item.index()){
+            hideLoading();
+          }
         });
+      },
+      change: function(event, ui) {
+      }
+    });
 
-        $( options.table+" tbody" ).disableSelection();
-    };
+    $( options.table+" tbody" ).disableSelection();
+  };
 
 })(jQuery);
